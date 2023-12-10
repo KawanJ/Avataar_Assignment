99fd3e64f703a5f8abc5b24b75708ff022582798
KawanJoshi <joshikawan@gmail.com>
2023-12-10 19:36:50 +0530

    Navbar Updates #2

--- avataar-app/src/Components/Navbar.js
+++ avataar-app/src/Components/Navbar.js
@@ -3,6 +3,13 @@ import './Navbar.css';
 
 
 const Navbar = () => {
+
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
   const [links, setLinks] = useState([
     { text: 'Item1', href: 'https://www.google.com' },
     { text: 'Item2', href: 'https://www.google.com' },
@@ -13,7 +20,9 @@ const Navbar = () => {
   const [moreLinks, setMoreLinks] = useState([]);
 
   const updateRightmostPosition = useCallback(() => {
-    //const navbarElement = document.querySelector('.navbar');
+    setDropdownOpen(false);
+
+    const navbarElement = document.querySelector('.navbar');
     const linksElement = document.querySelector('.links');
     const moreElement = document.querySelector('.more');
 
@@ -21,18 +30,25 @@ const Navbar = () => {
     if (moreLinks.length > 0) {
       const linkStyles = window.getComputedStyle(linksElement);
       const moreStyles = window.getComputedStyle(moreElement);
-      console.log(parseFloat(linkStyles.getPropertyValue('margin-right')) + parseFloat(moreStyles.getPropertyValue('margin-left')));
-      if(parseFloat(linkStyles.getPropertyValue('margin-right')) + parseFloat(moreStyles.getPropertyValue('margin-left')) >= 295) {
-        const newMoreLinks = [...moreLinks];
-        const movedLink = newMoreLinks.shift(); // Remove the first link from moreLinks
+      if(parseFloat(linkStyles.getPropertyValue('margin-right')) + parseFloat(moreStyles.getPropertyValue('margin-left')) >= 165 && links.length > 0) {
+        const newMoreLinks = moreLinks.slice(0, -1);
+        const movedLink = moreLinks[moreLinks.length - 1];
 
         setLinks((prevLinks) => [...prevLinks, movedLink]);
         setMoreLinks(newMoreLinks);
       }
+
+      if(parseFloat(linkStyles.getPropertyValue('margin-right')) + parseFloat(moreStyles.getPropertyValue('margin-left')) === 0) {
+        const newLinks = links.slice(0, -1);
+        const lastLink = links[links.length - 1];
+
+        setLinks(newLinks);
+        setMoreLinks((prevMoreLinks) => [...prevMoreLinks, lastLink]);
+      }
     }
 
     // Check if navbar size is less than links size and move last link to moreLinks
-    if (window.innerWidth < linksElement.getBoundingClientRect().right) {
+    if (navbarElement.getBoundingClientRect().right < linksElement.getBoundingClientRect().right && links.length > 0) {
       const newLinks = links.slice(0, -1);
       const lastLink = links[links.length - 1];
 
@@ -56,19 +72,25 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <h1 className="title"> Menu </h1>
-      <div className="links">
-        {links.map((link, index) => (
-          <a key={index} className="item" href={link.href}>
-            {link.text}
-          </a>
-        ))}
-      </div>
+        <div className="links">
+          {links.map((link, index) => (
+            <a key={index} className="item" href={link.href}>
+              {link.text}
+            </a>
+          ))}
+        </div>
       {moreLinks.length > 0 && (
-          <div className="more">
-            More
-            {/* Dropdown or other UI element for moreLinks */}
+        <div className="more" onClick={toggleDropdown}>
+          More
+          <div className={`dropdown-content ${isDropdownOpen ? 'show' : ''}`}>
+            {moreLinks.map((link, index) => (
+              <a key={index} href={link.href}>
+                {link.text}
+              </a>
+            ))}
           </div>
-        )}
+        </div>
+      )}
     </div>
   );
 };
