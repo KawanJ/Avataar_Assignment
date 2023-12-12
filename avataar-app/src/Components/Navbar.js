841ee72d75971cc9db2916d10d4935698aae4b71
KawanJoshi <joshikawan@gmail.com>
2023-12-12 23:10:40 +0530

    NavBar Update 3
    - Changed Css according to Figma
    - Have to add Search bar so had to change all the logic ;-;

--- avataar-app/src/Components/Navbar.js
+++ avataar-app/src/Components/Navbar.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import './Navbar.css';
+import logo from './..\\asset\\Intersectlogo.png'
+import searchIcon from './..\\asset\\search.png'
+import downArraw from './..\\asset\\Vector.png'
 
 
 const Navbar = () => {
@@ -11,10 +14,15 @@ const Navbar = () => {
   };
 
   const [links, setLinks] = useState([
-    { text: 'Item1', href: 'https://www.google.com' },
-    { text: 'Item2', href: 'https://www.google.com' },
-    { text: 'Item3', href: 'https://www.google.com' },
-    { text: 'Item4', href: 'https://www.google.com' },
+    { text: 'HOME', href: 'https://www.google.com' },
+    { text: 'ELECTRONICS', href: 'https://www.google.com' },
+    { text: 'MOVIES', href: 'https://www.google.com' },
+    { text: 'BOOK', href: 'https://www.google.com' },
+    { text: 'MUSIC', href: 'https://www.google.com' },
+    { text: 'CLOTHING', href: 'https://www.google.com' },
+    { text: 'GAMES', href: 'https://www.google.com' },
+    // { text: 'FURNITURE', href: 'https://www.google.com' },
+    // { text: 'ACCESSORIES', href: 'https://www.google.com' },
   ]);
 
   const [moreLinks, setMoreLinks] = useState([]);
@@ -22,15 +30,17 @@ const Navbar = () => {
   const updateRightmostPosition = useCallback(() => {
     setDropdownOpen(false);
 
-    const navbarElement = document.querySelector('.navbar');
+    // const navbarElement = document.querySelector('.navbar');
     const linksElement = document.querySelector('.links');
     const moreElement = document.querySelector('.more');
+    // const searchElement = document.querySelector('.search');
+
+    const linkStyles = window.getComputedStyle(linksElement);
 
     // Check if there is enough space to move an item from moreLinks to links
     if (moreLinks.length > 0) {
-      const linkStyles = window.getComputedStyle(linksElement);
       const moreStyles = window.getComputedStyle(moreElement);
-      if(parseFloat(linkStyles.getPropertyValue('margin-right')) + parseFloat(moreStyles.getPropertyValue('margin-left')) >= 165 && links.length > 0) {
+      if(parseFloat(linkStyles.getPropertyValue('margin-right')) + parseFloat(moreStyles.getPropertyValue('margin-left')) >= (moreLinks[moreLinks.length - 1].text.length * 7) + 90 && links.length > 0) {
         const newMoreLinks = moreLinks.slice(0, -1);
         const movedLink = moreLinks[moreLinks.length - 1];
 
@@ -38,7 +48,7 @@ const Navbar = () => {
         setMoreLinks(newMoreLinks);
       }
 
-      if(parseFloat(linkStyles.getPropertyValue('margin-right')) + parseFloat(moreStyles.getPropertyValue('margin-left')) === 0) {
+      if(parseFloat(linkStyles.getPropertyValue('margin-right')) + parseFloat(moreStyles.getPropertyValue('margin-left')) <= 16) {
         const newLinks = links.slice(0, -1);
         const lastLink = links[links.length - 1];
 
@@ -48,10 +58,11 @@ const Navbar = () => {
     }
 
     // Check if navbar size is less than links size and move last link to moreLinks
-    if (navbarElement.getBoundingClientRect().right < linksElement.getBoundingClientRect().right && links.length > 0) {
+    else if ( parseFloat(linkStyles.getPropertyValue('margin-right')) <= 24 && links.length > 0 && moreLinks.length === 0) {
       const newLinks = links.slice(0, -1);
       const lastLink = links[links.length - 1];
 
+      
       setLinks(newLinks);
       setMoreLinks((prevMoreLinks) => [...prevMoreLinks, lastLink]);
     }
@@ -71,17 +82,22 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
-      <h1 className="title"> Menu </h1>
-        <div className="links">
-          {links.map((link, index) => (
-            <a key={index} className="item" href={link.href}>
-              {link.text}
-            </a>
-          ))}
+      <div className="logo"> 
+        <img src={ logo } alt="Logo"></img>
+        <div className="logo-text">
+          E-COMM
         </div>
+      </div>
+      <div className="links">
+        {links.map((link, index) => (
+          <a key={index} className="item" href={link.href}>
+            {link.text}
+          </a>
+        ))}
+      </div>
       {moreLinks.length > 0 && (
         <div className="more" onClick={toggleDropdown}>
-          More
+          MORE <img src={ downArraw } alt="Arrow"></img>
           <div className={`dropdown-content ${isDropdownOpen ? 'show' : ''}`}>
             {moreLinks.map((link, index) => (
               <a key={index} href={link.href}>
@@ -91,6 +107,12 @@ const Navbar = () => {
           </div>
         </div>
       )}
+      <div className="search">
+        <img src={ searchIcon } alt="search icon"></img>
+        <div className="search-text">
+          Search Something 
+        </div>
+      </div>
     </div>
   );
 };
